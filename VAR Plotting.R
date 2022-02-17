186df66ef2c1f682f73deab3f1a6596154c6b800
Silu Muduli <79523464+silumuduli@users.noreply.github.com>
2022-02-17 16:35:32 +0530

    Update VAR Plotting.R

--- VAR Plotting.R
+++ VAR Plotting.R
@@ -1,6 +1,27 @@
 if (!require(pacman)) install.packages("pacman")
 pacman::p_load(tseries, tvReg, TSstudio, forecast, tibbletime, roll, gganimate, gapminder, rmdformats, ggpubr,ggplot2,readxl,RColorBrewer,data.table,astsa, foreign, haven,utils,changepoint,openxlsx,tsDyn,urca,KFAS,lubridate,forecast,vars,dplR, ggfortify)
 
+## Unit Root Tests
+unitroottest=function(d, lag){
+dts=ts(d, start = c(year,number),frequency = freq) 
+c=length(colnames(d))
+m=matrix(0,nrow=2, ncol=c)
+for(i in 1:c){
+m[1,i]=adf.test(dts[,i])$p.value
+m[2,i]=pp.test(dts[,i])$p.value
+}
+rownames(m)=c("ADF Test", "Phillips Perron Test")
+colnames(m)=colnames(d)
+return(m)
+}
+
+#x1=data$GDPGrowth
+#x2=data$MoneyGrowthm3
+#x3=data$Inflation
+#d=data.frame(x1,x2,x3)
+#colnames(d)=c("GDP Growth", "Real Money Growth", "Inflation")
+
+
 
 
 
